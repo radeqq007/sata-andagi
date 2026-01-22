0c3e38c461157441926d77db5824b42c68e4f2b7
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-01-22 17:17:22 +0100

    sata andagi

--- sata-andagi.sh
+++ sata-andagi.sh
@@ -4,6 +4,28 @@ sound_dir="./Sata Andagi.mp3"
 min_sleep=60  # 1 min
 max_sleep=300 # 5 min
 
+usage() {
+  echo "Usage: $0 [-m min_seconds] [-M max_seconds] [-f audio_file_path]"
+  exit 1
+}
+
+while getopts ":m:M:f:h" opt; do
+  case "$opt" in
+    m) min_sleep="$OPTARG" ;;
+    M) max_sleep="$OPTARG" ;;
+    f) sound_dir="$OPTARG" ;;
+    h) usage ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+    esac
+done
+
 mpv --no-terminal --no-video "$sound_dir" # play immediately
 
 while true; do
